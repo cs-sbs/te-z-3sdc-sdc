3bb6b55d10c8dac29e212ea39ec3cff1ab18f240
3sdc-sdc <2144347230@qq.com>
2024-10-31 16:14:49 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -9,32 +9,31 @@ public class NumberFeature {
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
         // 你的代码逻辑
-         scanner.close();
+       package org.example;
 
-        int result = 0;
-        int binaryValue = 1; // 从个位开始，二进制值的权重，初始为1（2^0）
-        int position = 1; // 位置编号，从1开始
+import java.util.Scanner;
 
-        // 将输入数字转换为字符串，以便逐位处理
-        String numberStr = Integer.toString(inputNumber);
-
-        // 从最高位到最低位遍历
-        for (int i = numberStr.length() - 1; i >= 0; i--) {
-            int digit = Character.getNumericValue(numberStr.charAt(i)); // 获取当前位的数字
-            boolean digitIsOdd = (digit % 2) != 0; // 判断数字是否为奇数
-            boolean positionIsOdd = (position % 2) != 0; // 判断位置是否为奇数
-
-            // 如果数字的奇偶性与位置的奇偶性相同，则记录为1（即加上当前的二进制权重值）
-            if (digitIsOdd == positionIsOdd) {
-                result += binaryValue;
+public class OddEven {
+    public static void main(String[] args) {
+        int number;
+        int countOdd = 0;
+        int countEven = 0;
+        Scanner in = new Scanner(System.in); // 数字用空格或换行隔开，每次读入一个，直到输入‘-1’
+        do{
+            number = in.nextInt();
+            if(number != -1)
+            {
+                if (number % 2 == 0) {
+                    countEven += 1;
+                } else {
+                    countOdd += 1;
+                }
             }
-
-            // 更新二进制权重值（左移一位，相当于乘以2）
-            binaryValue *= 2;
-
-            // 更新位置编号
-            position++;
-        }
+        }while(number != -1); // 理解do while和while循环的区别
+        System.out.print(countOdd+" ");
+        System.out.println(countEven);
+    }
+}
 
         System.out.println(result);
     }
